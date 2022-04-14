5396713ad7f5fcc0538cffb5b76fa393eee7d348
Andrew Gunn <andrewmgunn26@gmail.com>
2022-04-14 15:18:06 -0500

    ueah

---
+++ src/lab1/Driver.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class Driver {
+    public static void main(String[] args) {
+        Course course = new AdvancedJavaCourse("3","3");
+
+        System.out.println(course.toString());
+    }
+}
